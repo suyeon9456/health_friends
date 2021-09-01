b9af773fd23bd1927cb8a89f9dba0d6a9022548f
suyeon <suyeon9456@naver.com>
2021-09-01 17:40:22 +0900

    SearchFriends를 SearchGyms로 이름변경

---
+++ front/src/components/organisms/SearchGyms/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { TeamOutlined } from '@ant-design/icons';
+
+import { SearchHeader, SearchWrapper, SearchTitle, SearchFormWrapper, SearchListWrapper } from './style';
+import Input from '../../atoms/Input';
+import Item from '../../atoms/Item';
+
+const SearchGyms = () => {
+  const list = [{ title: '헬스장 1', description: '서울시 관악구 헬스장 주소 1', friends: 5 }, { title: '헬스장 2', description: '서울시 관악구 헬스장 주소 2', friends: 5 }, { title: '헬스장 3', description: '서울시 관악구 헬스장 주소 3', friends: 5 }];
+  return (
+    <SearchWrapper>
+      <SearchHeader>
+        <span>10개의 헬스장</span>
+        <SearchTitle>서울 관악구 검색 결과</SearchTitle>
+      </SearchHeader>
+      <SearchFormWrapper>
+        <Input type="search" placeholder="관심 헬스장을 검색해보세요." enterButton />
+      </SearchFormWrapper>
+      <SearchListWrapper>
+        {/* <List list={list} /> */}
+        {list.map((item, i) => (
+          <Item
+            key={i}
+            title={item.title}
+            description={(
+              <div>
+                <span>{item.description}</span>
+                <div>
+                  <TeamOutlined /> 5명
+                </div>
+              </div>
+            )}
+          />
+        ))}
+      </SearchListWrapper>
+    </SearchWrapper>
+  );
+};
+
+export default SearchGyms;
