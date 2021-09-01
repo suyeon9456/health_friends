afb5c40d18456ae7ac7fd3e8c85cbb2efd93443a
suyeon <suyeon9456@naver.com>
2021-09-01 17:40:39 +0900

    SearchMap component 작업진행중

---
+++ front/src/components/organisms/SearchMap/index.js
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+
+import { MapWrapper } from './style';
+
+const SearchMap = () => {
+  useEffect(() => {
+    const container = document.getElementById('kakaoMap');
+    const options = {
+      // eslint-disable-next-line no-undef
+      center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+      level: 3, // 지도의 레벨(확대, 축소 정도)
+    };
+
+    // eslint-disable-next-line no-undef
+    const map = new kakao.maps.Map(container, options);
+
+    // 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성
+    const mapTypeControl = new kakao.maps.MapTypeControl();
+    map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+
+    // 지도 확대 축소를 제어할 수 있는 줌 컨트롤을 생성
+    const zoomControl = new kakao.maps.ZoomControl();
+    map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+  }, []);
+
+  return (
+    <MapWrapper>
+      <div id="kakaoMap" style={{ width: '100%', height: '550px' }} />
+    </MapWrapper>
+  );
+};
+
+export default SearchMap;
---
+++ front/src/components/organisms/SearchMap/style.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const MapWrapper = styled.section`
+  display: flex;
+  /* align-items: center; */
+  flex-direction: column;
+  flex-wrap: nowrap;
+  margin-left: 10px;
+  width: 100% - 10px;
+  background-color: #ffffff;
+  border-radius: 5px;
+  border: 1px solid #f0f0f0;
+  margin-top: 10px;
+  overflow-y: auto;
+  max-height: 100vh;
+  overflow: hidden;
+`;
