0b1c89b5ebc0d104316cf9a2c74be9769929641c
suyeon <suyeon9456@naver.com>
2021-09-07 16:47:10 +0900

    SearchGyms 리스트 drawer 기능 추가 및 @media 추가

--- front/src/components/organisms/SearchGyms/index.js
+++ front/src/components/organisms/SearchGyms/index.js
@@ -1,23 +1,35 @@
 import React, { useEffect, useState } from 'react';
-import { TeamOutlined } from '@ant-design/icons';
+import { LeftOutlined, RightOutlined, TeamOutlined, ZoomInOutlined } from '@ant-design/icons';
+import PropTypes from 'prop-types';
 
-import { SearchHeader, SearchWrapper, SearchTitle, SearchFormWrapper, SearchListWrapper } from './style';
+import {
+  SearchHeader,
+  SearchWrapper,
+  SearchTitle,
+  SearchFormWrapper,
+  SearchListWrapper,
+  GymWrapper, FoldButton,
+  SearchSidebar,
+} from './style';
 import Input from '../../atoms/Input';
 import Item from '../../atoms/Item';
+import SearchFriends from '../SearchFriends';
+import Avatar from '../../atoms/Avatar';
+import Button from '../../atoms/Button';
 
-const SearchGyms = () => {
+const SearchGyms = ({ foldedGym, changeFoldedGym }) => {
   const [browserHeight, setBrowserHeight] = useState('');
   const list = [
-    { title: '헬스장 1', description: '서울시 관악구 헬스장 주소 1', friends: 5 },
-    { title: '헬스장 2', description: '서울시 관악구 헬스장 주소 2', friends: 5 },
-    { title: '헬스장 3', description: '서울시 관악구 헬스장 주소 3', friends: 5 },
-    { title: '헬스장 4', description: '서울시 관악구 헬스장 주소 3', friends: 5 },
-    { title: '헬스장 5', description: '서울시 관악구 헬스장 주소 3', friends: 5 },
-    { title: '헬스장 6', description: '서울시 관악구 헬스장 주소 3', friends: 5 },
-    { title: '헬스장 7', description: '서울시 관악구 헬스장 주소 3', friends: 5 },
-    { title: '헬스장 8', description: '서울시 관악구 헬스장 주소 3', friends: 5 },
-    { title: '헬스장 9', description: '서울시 관악구 헬스장 주소 3', friends: 5 },
-    { title: '헬스장 10', description: '서울시 관악구 헬스장 주소 3', friends: 6 },
+    { id: 1, title: '헬스장 1', description: '서울시 관악구 헬스장 주소 1', friends: 5 },
+    { id: 2, title: '헬스장 2', description: '서울시 관악구 헬스장 주소 2', friends: 5 },
+    { id: 3, title: '헬스장 3', description: '서울시 관악구 헬스장 주소 3', friends: 5 },
+    { id: 4, title: '헬스장 4', description: '서울시 관악구 헬스장 주소 3', friends: 5 },
+    // { id: 5, title: '헬스장 5', description: '서울시 관악구 헬스장 주소 3', friends: 5 },
+    // { id: 6, title: '헬스장 6', description: '서울시 관악구 헬스장 주소 3', friends: 5 },
+    // { id: 7, title: '헬스장 7', description: '서울시 관악구 헬스장 주소 3', friends: 5 },
+    // { id: 8, title: '헬스장 8', description: '서울시 관악구 헬스장 주소 3', friends: 5 },
+    // { id: 9, title: '헬스장 9', description: '서울시 관악구 헬스장 주소 3', friends: 5 },
+    // { id: 10, title: '헬스장 10', description: '서울시 관악구 헬스장 주소 3', friends: 6 },
   ];
 
   useEffect(() => {
@@ -25,36 +37,65 @@ const SearchGyms = () => {
   }, [browserHeight]);
 
   return (
-    <SearchWrapper>
-      {/* <SearchFriends /> */}
-      <SearchHeader>
-        <span>10개의 헬스장</span>
-        <SearchTitle>서울 관악구 검색 결과</SearchTitle>
-      </SearchHeader>
-      <SearchFormWrapper>
-        <Input type="search" placeholder="관심 헬스장을 검색해보세요." enterButton />
-      </SearchFormWrapper>
-      <SearchListWrapper
-        browserHeight={browserHeight}
+    <SearchWrapper
+      foldedGym={foldedGym}
+    >
+      <SearchSidebar
+        foldedGym={foldedGym}
       >
-        {/* <List list={list} /> */}
-        {list.map((item, i) => (
-          <Item
-            key={i}
-            title={item.title}
-            description={(
-              <div>
-                <span>{item.description}</span>
-                <div>
-                  <TeamOutlined /> {item.friends}명
-                </div>
-              </div>
-            )}
+        <div>
+          <Avatar size="small" />
+          <Button
+            icon={<ZoomInOutlined />}
+            type="text"
           />
-        ))}
-      </SearchListWrapper>
+        </div>
+      </SearchSidebar>
+      <FoldButton
+        foldedGym={foldedGym}
+        onClick={changeFoldedGym}
+      >
+        {foldedGym ? <RightOutlined /> : <LeftOutlined />}
+      </FoldButton>
+      <GymWrapper
+        foldedGym={foldedGym}
+      >
+        <SearchHeader>
+          <span>10개의 헬스장</span>
+          <SearchTitle>서울 관악구 검색 결과</SearchTitle>
+        </SearchHeader>
+        <SearchFormWrapper>
+          <Input type="search" placeholder="관심 헬스장을 검색해보세요." enterButton />
+        </SearchFormWrapper>
+        <SearchListWrapper
+          browserHeight={browserHeight}
+        >
+          {list.map((item) => (
+            <Item
+              key={item.id}
+              title={item.title}
+              description={(
+                <div>
+                  <span>{item.description}</span>
+                  <div>
+                    <TeamOutlined /> {item.friends}명
+                  </div>
+                </div>
+              )}
+            />
+          ))}
+        </SearchListWrapper>
+      </GymWrapper>
+      <SearchFriends
+        foldedGym={foldedGym}
+      />
     </SearchWrapper>
   );
+};
+
+SearchGyms.propTypes = {
+  foldedGym: PropTypes.bool.isRequired,
+  changeFoldedGym: PropTypes.func,
 };
 
 export default SearchGyms;
--- front/src/components/organisms/SearchGyms/style.js
+++ front/src/components/organisms/SearchGyms/style.js
@@ -1,20 +1,141 @@
 import styled, { css } from 'styled-components';
 
 export const SearchWrapper = styled.section`
-  display: flex;
-  /* align-items: center; */
-  flex-direction: column;
-  flex-wrap: nowrap;
   width: 100%;
-  background-color: #ffffff;
-  box-sizing: border-box;
+  margin-top: 10px;
   border-radius: 5px;
   border: 1px solid #f0f0f0;
-  margin-top: 10px;
-  overflow-y: auto;
-  height: 100%;
-  /* max-height: 100vh; */
   overflow: hidden;
+  box-sizing: border-box;
+
+  @media (min-width: 768px) {
+    height: 100%;
+  }
+`;
+
+export const SearchSidebar = styled.div`
+  position: relative;
+  width: 100%;
+  height: 43px;
+  padding: 5px 24px;
+  background-color: #fff;
+  border-radius: 5px;
+  border-bottom: 1px solid #f0f0f0;
+  z-index: 10;
+
+  & > div {
+      height: 100%;
+      overflow: hidden;
+      & > span {
+        margin-top: 4px;
+      }
+      & > button {
+        float: right;
+        padding: 0;
+        color: #00000073;
+        font-size: 16px;
+        padding-left: 7px;
+        ${({ foldedGym }) => !foldedGym && css`
+          color: #b37feb;
+        `}
+      }
+    }
+  
+  @media (min-width: 768px) {
+    border: 0;
+    width: 40px;
+    height: 100%;
+    padding: 16px 0;
+    float: left;
+    text-align: center;
+    border-right: 1px solid #f0f0f0;
+    & > div {
+      & > span {
+        margin: 0;
+        margin-bottom: 10px;
+      }
+  
+      & > button {
+        width: 100%;
+        font-size: 16px;
+        padding-left: 7px;
+      }
+    }
+  }
+`;
+
+export const FoldButton = styled.div`
+  position: absolute;
+  width: 20px;
+  height: 60px;
+  left: 50%;
+  top: calc(100% - 20px);
+  background-color: #fff;
+  border-radius: 0 5px 5px 0;
+  border: 1px solid #f0f0f0;
+  border-left: none;
+  text-align: center;
+  box-shadow: 0 2px 8px #e8e9e9;
+  cursor: pointer;
+  transition: all .35s;
+  transform: rotate( 90deg );
+  z-index: 100;
+
+  & > span {
+    color: #00000073;
+    line-height: 56px;
+  }
+  &:hover {
+    & > span {
+      color: #b37feb;
+    }
+  }
+  ${({ foldedGym }) => foldedGym && css`
+    top: 34px !important;
+  `}
+
+  @media (min-width: 768px) {
+    width: 20px;
+    height: 60px;
+    left: 100%;
+    top: calc(50% - 50px + 10px);
+    transform: none;
+    ${({ foldedGym }) => foldedGym && css`
+      top: calc(50% - 50px + 10px) !important;
+      left: 41px !important;
+    `}
+  }
+`;
+
+export const GymWrapper = styled.div`
+  position: relative;
+  display: flex;
+  width: 100%;
+  left: 0;
+  top: 0;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  background-color: #ffffff;
+  box-sizing: border-box;
+  overflow-y: auto;
+  overflow: hidden;
+  border-radius: 0 0 5px 5px;
+  z-index: 6;
+  transition: all .35s;
+
+  ${({ foldedGym }) => foldedGym && css`
+    top: -600px;
+  `}
+  
+  @media (min-width: 768px) {
+    width: calc(100% - 40px);
+    height: 100%;
+    border-radius: 0 5px 5px 0;
+    ${({ foldedGym }) => foldedGym && css`
+      top: 0; 
+      left: -500px;
+    `}
+  }
 `;
 
 export const SearchHeader = styled.div`
@@ -51,11 +172,16 @@ export const SearchFormWrapper = styled.div`
 export const SearchListWrapper = styled.div`
   position: relative;
   width: 100%;
+  max-height: 390px;
   padding: 0 24px;
   color: #000000d9;
   background: #fff;
-  ${({ browserHeight }) => browserHeight && css`
-    height: ${browserHeight - 246}px;
-  `}
+  min-height: 390px;
+  @media (min-width: 768px) {
+    max-height: none;
+    ${({ browserHeight }) => browserHeight && css`
+      height: ${browserHeight - 246}px;
+    `}
+  }
   overflow-y: auto;
 `;
