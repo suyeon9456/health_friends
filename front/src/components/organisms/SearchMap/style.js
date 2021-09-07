ba403ef878ec6a10723bf3ac67e9f572f99b6ea1
suyeon <suyeon9456@naver.com>
2021-09-07 16:47:27 +0900

    SearchMap media 추가

--- front/src/components/organisms/SearchMap/style.js
+++ front/src/components/organisms/SearchMap/style.js
@@ -2,14 +2,18 @@ import styled from 'styled-components';
 
 export const MapWrapper = styled.section`
   display: flex;
-  /* align-items: center; */
   flex-direction: column;
   flex-wrap: nowrap;
-  margin-left: 10px;
-  width: 100% - 10px;
+  width: 100%;
+  height: 500px;
   background-color: #ffffff;
   border-radius: 5px;
   border: 1px solid #f0f0f0;
   margin-top: 10px;
-  height: 100%;
+  
+  @media (min-width: 768px) {
+    width: 100% - 10px;
+    height: 100%;
+    margin-left: 10px;
+  }
 `;
