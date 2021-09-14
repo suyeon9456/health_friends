015f1da85e483d8863122962b71225c0b98c6d26
suyeon <suyeon9456@naver.com>
2021-09-14 14:50:36 +0900

    ModalRequestFriend component 작업완료

---
+++ front/src/components/organisms/ModalRequestFriend/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import { MatchingInfoWrap,
+  InfoContent,
+  RequestFriendWrap,
+  UserInfoWrap,
+  Content,
+  DescriptionWrap } from './style';
+import FormDatePicker from '../../molecules/FormDatePicker';
+import FormInput from '../../molecules/FormInput';
+import FormTextarea from '../../molecules/FormTextarea';
+import Avatar from '../../atoms/Avatar';
+
+const ModalRequestFriend = () => (
+  <RequestFriendWrap>
+    <MatchingInfoWrap>
+      <h4>매칭정보</h4>
+      <FormDatePicker
+        label="날짜"
+      />
+      <FormInput
+        label="주소"
+      />
+    </MatchingInfoWrap>
+    <UserInfoWrap>
+      <InfoContent id="my_info">
+        <h4>내정보</h4>
+        <Content>
+          <Avatar size={62} />
+          <div>
+            <div className="nickname">닉네임</div>
+            <div>간단소개</div>
+          </div>
+        </Content>
+      </InfoContent>
+      <InfoContent id="friend_info">
+        <h4>친구정보</h4>
+        <Content>
+          <Avatar size={62} />
+          <div>
+            <div className="nickname">닉네임</div>
+            <div>간단소개</div>
+          </div>
+        </Content>
+      </InfoContent>
+    </UserInfoWrap>
+    <DescriptionWrap>
+      <h4>요청 또는 전하고 싶은 말</h4>
+      <FormTextarea />
+    </DescriptionWrap>
+  </RequestFriendWrap>
+);
+
+export default ModalRequestFriend;
---
+++ front/src/components/organisms/ModalRequestFriend/style.js
@@ -0,0 +1,77 @@
+import styled from 'styled-components';
+
+export const RequestFriendWrap = styled.div`
+  width: 100%;
+  height: 420px;
+  padding: 10px 24px;
+  font-variant: tabular-nums;
+  font-feature-settings: 'tnum';
+`;
+
+export const MatchingInfoWrap = styled.div`
+  width: 100%;
+  margin-bottom: 10px;
+
+  & > h4 {
+    font-size: 14px;
+    font-weight: 600;
+  }
+
+  & > div {
+    &:not(:last-child) {
+      margin-bottom: 5px;
+    }
+    & > label {
+      color: #00000073;
+      font-weight: 500;
+      font-size: 14px;
+    }
+    & > * {
+      margin-top: 5px;
+    }
+  }
+`;
+
+export const UserInfoWrap = styled.div`
+  width: 100%;
+  display: flex;
+  margin-bottom: 10px;
+
+  & > div {
+    flex-grow: 1;
+  }
+`;
+
+export const InfoContent = styled.div`
+  & > h4 {
+    font-size: 14px;
+    font-weight: 600;
+  }
+`;
+
+export const Content = styled.div`
+  display: inline-flex;
+  width: 100%;
+
+  & div {
+    color: #00000073;
+    font-weight: 500;
+    font-size: 14px;
+  }
+  & .nickname {
+    color: #000000d9;
+    font-weight: 700;
+  }
+  & > div {
+    padding: 5px;
+  }
+`;
+
+export const DescriptionWrap = styled.div`
+  width: 100%;
+
+  & > h4 {
+    font-size: 14px;
+    font-weight: 600;
+  }
+`;
