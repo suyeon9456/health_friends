cb3b53a7fae5ef516c7fd638fd57eccee9977025
suyeon <suyeon9456@naver.com>
2021-12-21 17:10:12 +0900

    console 테스트

--- back/models/index.js
+++ back/models/index.js
@@ -3,6 +3,8 @@ const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config')[env];
 const db = {};
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
+console.log(sequelize);
+console.log(config);
 
 db.User = require('./user')(sequelize, Sequelize);
 db.Gym = require('./gym')(sequelize, Sequelize);
