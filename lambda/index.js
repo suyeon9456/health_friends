c9cae96700c2693818085c2f12396951ea9f8983
suyeon <suyeon9456@naver.com>
2022-01-06 18:46:22 +0900

    fix: lamda 함수 코드 수정

--- lambda/index.js
+++ lambda/index.js
@@ -1,15 +1,15 @@
 const AWS = require('aws-sdk');
 const sharp = require('sharp');
 
-const s3 = AWS.S3();
+const s3 = new AWS.S3();
 
 exports.handler = async (event, context, callback) => {
-  const Bucket = event.Records[0].s3.bucket.name; // health-friends-s3
-  const Key = decodeURIComponent(event.Records[0].s3.object.key); // original/123456_abc.png
-  console.long(Bucket, key);
-  const filename = Key.split('/')[Key.split('/').length - 1]; // 파일 이름만 추출
-  const ext = Key.split('.')[Key.split('.').length - 1].toLowerCase(); // 확장자
-  const requiredFormat = ext === 'jpg' ? 'jpeg' : ext; // 확장자가 jpg일 경우만 jpeg로
+  const Bucket = event.Records[0].s3.bucket.name; // react-nodebird-s3
+  const Key = decodeURIComponent(event.Records[0].s3.object.key); // original/12312312_abc.png
+  console.log(Bucket, Key);
+  const filename = Key.split('/')[Key.split('/').length - 1];
+  const ext = Key.split('.')[Key.split('.').length - 1].toLowerCase();
+  const requiredFormat = ext === 'jpg' ? 'jpeg' : ext;
   console.log('filename', filename, 'ext', ext);
 
   try {
@@ -25,7 +25,6 @@ exports.handler = async (event, context, callback) => {
       Body: resizedImage,
     }).promise();
     console.log('put', resizedImage.length);
-
     return callback(null, `thumb/${filename}`);
   } catch (error) {
     console.error(error);
