6fe47e62ba3efb113993457efc395c670d9737b0
suyeon <suyeon9456@naver.com>
2022-02-09 15:15:45 +0900

    typescript migration

--- front/store/contextStore.ts
+++ front/store/contextStore.ts
@@ -1,9 +1,20 @@
 import { createContext, useContext } from 'react';
 
-export const ShowStateContext = createContext(null);
-export const ShowDispatchContext = createContext(null);
+// 상태를 위한 타입
+type InitialState = {
+  drawerShow: boolean,
+  alertShow: boolean,
+  modalShow: boolean,
+  editNickname: boolean,
+  editDescription: boolean,
+};
 
-export const initialState = {
+type Action =
+  | { type: 'CHANGE_STATE'; value: boolean }
+  | { type: 'CHANGE_STATE_ALERT'; value: boolean }
+  | { type: 'CHANGE_STATE_MODAL'; value: boolean };
+
+export const initialState: InitialState = {
   drawerShow: false,
   alertShow: false,
   modalShow: false,
@@ -11,7 +22,10 @@ export const initialState = {
   editDescription: false,
 };
 
-export const reducer = (state = initialState, action) => {
+export const ShowStateContext = createContext<InitialState | null>(null);
+export const ShowDispatchContext = createContext<React.Dispatch<Action> | null>(null);
+
+export const reducer = (state = initialState, action: Action) => {
   switch (action.type) {
     case 'CHANGE_STATE':
       return {
