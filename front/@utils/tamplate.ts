eab5c25a9b1572dd6ed9a1aba91077dbe425ddfa
suyeon <suyeon9456@naver.com>
2022-04-29 16:26:47 +0900

    refactor: map overlay template 분리

---
+++ front/@utils/tamplate.ts
@@ -0,0 +1,75 @@
+import styles from '@/scss/searchMap.module.scss';
+import { userIcon } from './svg';
+
+export const overlayContainer = ({
+  name,
+  address,
+}: {
+  name: string;
+  address: string;
+}) => {
+  const container = `
+  <div class=${styles.contentArrow}>
+    <span class=${styles.arrow} />
+  </div>
+  <div class=${styles.contentInner}>
+    <div class=${styles.inner}>
+      <div class=${styles.innerTitle}>
+        ${name}
+      </div>
+      <div class=${styles.innerBody}>
+        ${address}
+      </div>
+    </div>
+  </div>`;
+  return container;
+};
+
+export const avatarContainer = (member: any[]) => {
+  const avatarGroup = document.createElement('div');
+  avatarGroup.className = `${styles.avatarGroup}`;
+
+  const anticon = document.createElement('a');
+  anticon.className = `${styles.anticon}`;
+  anticon.innerHTML = userIcon;
+  anticon.addEventListener('click', () => {
+    console.log('click');
+  });
+
+  if (member.length > 3) {
+    const plusAvatar = document.createElement('span');
+    plusAvatar.className = `${styles.plusAvatar}`;
+    plusAvatar.innerText = `+ ${member.length - 3}`;
+    member.forEach((user, i) => {
+      const avatar = document.createElement('span');
+      avatar.className = `${styles.avatar}`;
+      if (i <= 3) {
+        if (user.Image) {
+          const avatarImage = document.createElement('img');
+          avatarImage.className = `${styles.avatarImage}`;
+          avatarImage.src = user.Image?.src;
+          avatar.appendChild(avatarImage);
+        } else {
+          avatar.appendChild(anticon);
+        }
+        avatarGroup.appendChild(avatar);
+      }
+    });
+    avatarGroup.appendChild(plusAvatar);
+  } else {
+    member.forEach((user) => {
+      const avatar = document.createElement('span');
+      avatar.className = `${styles.avatar}`;
+      if (user.Image) {
+        const avatarImage = document.createElement('img');
+        avatarImage.className = `${styles.avatarImage}`;
+        avatarImage.src = user.Image?.src;
+        avatar.appendChild(avatarImage);
+      } else {
+        avatar.appendChild(anticon);
+      }
+      avatarGroup.appendChild(avatar);
+    });
+  }
+  return avatarGroup;
+};
--- front/src/components/organisms/SearchMap/index.tsx
+++ front/src/components/organisms/SearchMap/index.tsx
@@ -10,6 +10,8 @@ import { gymAndFriendsByIdKey } from '@/../@utils/queryKey';
 import { ButtonType } from '@/../@types/utils';
 import { Location } from '@/../@types/gym';
 import { SearchMapProps } from '@/../@types/map';
+import { userIcon } from '@/../@utils/svg';
+import { avatarContainer, overlayContainer } from '@/../@utils/tamplate';
 import { Button, Icon } from '../../atoms';
 import { MapWrap } from './style';
 
@@ -62,13 +64,13 @@ const SearchMap = ({ foldedFriends, setFoldedFriends }: SearchMapProps) => {
 
     // 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성
     const mapTypeControl = new (window as any).kakao.maps.MapTypeControl();
-    map?.current?.addControl(
+    map.current?.addControl(
       mapTypeControl,
       (window as any).kakao.maps.ControlPosition.TOPRIGHT
     );
     // 지도 확대 축소를 제어할 수 있는 줌 컨트롤을 생성
     const zoomControl = new (window as any).kakao.maps.ZoomControl();
-    map?.current?.addControl(
+    map.current?.addControl(
       zoomControl,
       (window as any).kakao.maps.ControlPosition.RIGHT
     );
@@ -114,107 +116,18 @@ const SearchMap = ({ foldedFriends, setFoldedFriends }: SearchMapProps) => {
       const moveLatLon = new (window as any).kakao.maps.LatLng(lat, lon);
       map.current.panTo(moveLatLon);
 
-      const anticonUser =
-        '<svg viewBox="64 64 896 896" focusable="false" fill="currentColor" width="1em" height="1em"><path d="M858.5 763.6a374 374 0 00-80.6-119.5 375.63 375.63 0 00-119.5-80.6c-.4-.2-.8-.3-1.2-.5C719.5 518 760 444.7 760 362c0-137-111-248-248-248S264 225 264 362c0 82.7 40.5 156 102.8 201.1-.4.2-.8.3-1.2.5-44.8 18.9-85 46-119.5 80.6a375.63 375.63 0 00-80.6 119.5A371.7 371.7 0 00136 901.8a8 8 0 008 8.2h60c4.4 0 7.9-3.5 8-7.8 2-77.2 33-149.5 87.8-204.3 56.7-56.7 132-87.9 212.2-87.9s155.5 31.2 212.2 87.9C779 752.7 810 825 812 902.2c.1 4.4 3.6 7.8 8 7.8h60a8 8 0 008-8.2c-1-47.8-10.9-94.3-29.5-138.2zM512 534c-45.9 0-89.1-17.9-121.6-50.4S340 407.9 340 362c0-45.9 17.9-89.1 50.4-121.6S466.1 190 512 190s89.1 17.9 121.6 50.4S684 316.1 684 362c0 45.9-17.9 89.1-50.4 121.6S557.9 534 512 534z" /></svg>';
-
       const contentWrap = document.createElement('div');
       contentWrap.className = `${styles.contentWrap}`;
+      contentWrap.innerHTML = overlayContainer({
+        name: gym.name,
+        address: gym.address,
+      });
+      const avatarGroup = avatarContainer(gym?.Users);
+      contentWrap.querySelector(`.${styles.inner}`)?.prepend(avatarGroup);
 
-      const contentArrow = document.createElement('div');
-      contentArrow.className = `${styles.contentArrow}`;
-
-      const arrow = document.createElement('span');
-      arrow.className = `${styles.arrow}`;
-
-      const contentInner = document.createElement('div');
-      contentInner.className = `${styles.contentInner}`;
-
-      const inner = document.createElement('div');
-      inner.className = `${styles.inner}`;
-
-      const avatarGroup = document.createElement('div');
-      avatarGroup.className = `${styles.avatarGroup}`;
-
-      if (gym?.Users?.length > 3) {
-        const anticon = document.createElement('a');
-        anticon.className = `${styles.anticon}`;
-        anticon.innerHTML = anticonUser;
-
-        anticon.addEventListener('click', () => {
-          console.log('click');
-        });
-
-        const avatar = document.createElement('span');
-        avatar.className = `${styles.avatar}`;
-
-        const plusAvatar = document.createElement('span');
-        plusAvatar.className = `${styles.plusAvatar}`;
-        plusAvatar.innerText = `+ ${gym?.Users.length - 3}`;
-
-        gym.Users.forEach(
-          (
-            user: {
-              Image?: { src: string };
-            },
-            i: number
-          ) => {
-            if (i <= 3) {
-              if (user.Image) {
-                const avatarImage = document.createElement('img');
-                avatarImage.className = `${styles.avatarImage}`;
-                avatarImage.src = user.Image?.src;
-                avatar.appendChild(avatarImage);
-              } else {
-                avatar.appendChild(anticon);
-              }
-              avatarGroup.appendChild(avatar);
-            }
-          }
-        );
-        avatarGroup.appendChild(plusAvatar);
-      } else {
-        gym.Users?.forEach((user: { Image?: { src: string } }) => {
-          const anticon = document.createElement('a');
-          anticon.className = `${styles.anticon}`;
-          anticon.innerHTML = anticonUser;
-          anticon.addEventListener('click', () => {
-            console.log('click');
-          });
-          const avatar = document.createElement('span');
-          avatar.className = `${styles.avatar}`;
-          if (user.Image) {
-            const avatarImage = document.createElement('img');
-            avatarImage.className = `${styles.avatarImage}`;
-            avatarImage.src = user.Image?.src;
-            avatar.appendChild(avatarImage);
-          } else {
-            avatar.appendChild(anticon);
-          }
-          avatarGroup.appendChild(avatar);
-        });
-      }
-
-      const innerTitle = document.createElement('div');
-      innerTitle.className = `${styles.innerTitle}`;
-      innerTitle.appendChild(document.createTextNode(gym.name));
-
-      const innerBody = document.createElement('div');
-      innerBody.className = `${styles.innerBody}`;
-      innerBody.appendChild(document.createTextNode(gym.address));
-
-      contentArrow.appendChild(arrow);
-      contentWrap.appendChild(contentArrow);
-
-      inner.appendChild(avatarGroup);
-      inner.appendChild(innerTitle);
-      inner.appendChild(innerBody);
-      contentInner.appendChild(inner);
-      contentWrap.appendChild(contentInner);
-
-      const overlayContent = contentWrap;
       customOverlay.current = new (window as any).kakao.maps.CustomOverlay({
         position: moveLatLon,
-        content: overlayContent,
+        content: contentWrap,
         xAnchor: 0.5,
         yAnchor: 0.96,
       });
